bb54e18b0b30f9e68609a8b66f15bae9d18f6f61
ThomasWega <tomous.wega@gmail.com>
2022-09-16 23:46:47 +0200

    1.0 - added slot limit, staff permission, messages, ...

--- src/main/java/me/wega/Listeners.java
+++ src/main/java/me/wega/Listeners.java
@@ -4,23 +4,56 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class Listeners implements Listener {
 
     private final Main mainClass;
 
-    public Listeners(Main main){
+    public Listeners(Main main) {
         this.mainClass = main;
     }
 
     @EventHandler
-    public void onPlayerLoginEvent(PlayerLoginEvent event){
+    public void onPlayerLoginEvent(PlayerLoginEvent event) {
         if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
-             if (event.getPlayer().hasPermission(mainClass.getConfig().getString("permission"))){
-                event.allow();
-            }else if (!event.getPlayer().hasPermission(mainClass.getConfig().getString("permission"))){
-                 event.setKickMessage(ChatColor.translateAlternateColorCodes('&', String.join("\n", mainClass.getConfig().getStringList("kick-message"))));
-             }
+            if (event.getPlayer().hasPermission(mainClass.getConfig().getString("permission.player"))) {
+                if (mainClass.getConfig().getBoolean("limit-slots.enable", true)) {
+                    // check if players online are not larger int than max player + max patronslots limit in config
+                    if ((mainClass.getServer().getMaxPlayers() + mainClass.getConfig().getInt("limit-slots.max")) > mainClass.getServer().getOnlinePlayers().size()) {
+                        event.allow();
+                    }
+                    // in case of slot limit being disabled, check if limit-slot.enabled isn't true (it can be everything else other than true)
+                } else if (!mainClass.getConfig().getBoolean("limit-slots.enable", true)) {
+                    event.allow();
+                }
+                if (mainClass.getConfig().getBoolean("join-message.enable", true)) { // check if join message is enabled
+                    // Delay the sendMessage to player using BukkitRunnable
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            // alternate the color codes and join the lines with \n, for the config to work properly
+                            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', String.join("\n", mainClass.getConfig().getStringList("join-message.message"))));
+                        }
+                        // get the delay time from config in seconds and multiply it by 20, to get it in ticks
+                    }.runTaskLater(this.mainClass, mainClass.getConfig().getLong("join-message.delay") * 20L);
+                }
+            }
+            if (mainClass.getConfig().getBoolean("kick-message.enable", true)) {
+                // alternate the color codes and join the lines with \n, for the config to work properly
+                event.setKickMessage(ChatColor.translateAlternateColorCodes('&', String.join("\n", mainClass.getConfig().getStringList("kick-message.message"))));
+            }
         }
     }
+
+    // Listener for staff. Staff can bypass the limit-slots.max, also it doesn't get the join message
+    @EventHandler
+    public void onStaffLoginEvent(PlayerLoginEvent event) {
+        if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL) {
+            if (event.getPlayer().hasPermission(mainClass.getConfig().getString("permission.staff"))) {
+                event.allow();
+            }
+        }
+    }
+
 }
--- src/main/java/me/wega/Main.java
+++ src/main/java/me/wega/Main.java
@@ -8,27 +8,42 @@ import java.util.List;
 public final class Main extends JavaPlugin {
 
     /* TODO
-    - ADD MORE PERMS OPTIONS AND LIMIT TO THEM
-    - ADD PLAYER MESSAGE AFTER HE JOINS, THAT HE BYPASSED THE SLOT LIMIT
+    - PUSH THE CHANGES TO GITHUB
      */
 
     public Listeners listeners;
 
     @Override
     public void onEnable() {
+
         listeners = new Listeners(this);
 
+        // register events
         getServer().getPluginManager().registerEvents(new Listeners(this), this);
 
-        getConfig().addDefault("permission", "wega.patronslots");
+        // add config defaults for permissions and limits
+        getConfig().addDefault("permission.player", "wega.patronslots.player");
+        getConfig().addDefault("permission.staff", "wega.patronslots.staff");
+        getConfig().addDefault("limit-slots.enable", true);
+        getConfig().addDefault("limit-slots.max", 20);
 
+        // add config defaults for kick message
+        getConfig().addDefault("kick-message.enable", true);
         List<String> KickMessageList = new ArrayList<>();
         KickMessageList.add("&cBuy VIP to access full server!");
         KickMessageList.add("");
         KickMessageList.add("&8store.ratethisplugin.com");
-        getConfig().addDefault("kick-message", KickMessageList);
+        getConfig().addDefault("kick-message.message", KickMessageList);
 
+        // add config defaults for join message
+        getConfig().addDefault("join-message.enable", true);
+        getConfig().addDefault("join-message.delay", 1.5);
+        List<String> JoinMessageList = new ArrayList<>();
+        JoinMessageList.add("&aYou just successfully bypassed the slot limit, because of your VIP!");
+        JoinMessageList.add("&fThanks for supporting us! <3");
+        getConfig().addDefault("join-message.message", JoinMessageList);
 
+        // save the config
         getConfig().options().copyDefaults(true);
         saveConfig();
     }
